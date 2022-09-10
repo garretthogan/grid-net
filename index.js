f798bbb75f88067e557f2f97a0d2ba3b15bfa6c3
Garrett Hogan <garretthogan@garretts-mbp.lan>
2022-09-10 12:58:38 -0700

    changed port back to env var

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.on('upgrade', (req, socket, head) => {
 	});
 });
 
-const port = 80;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
 	console.log(`listening on *:${port}`);
 });
