5b4e5dcebe3c9f979fa9d6ea2852ab9dba0963d5
Garrett Hogan <garretthogan@garretts-mbp.lan>
2022-09-10 11:53:08 -0700

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const crypto = require('crypto');
+const WebSocket = require('ws');
+
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ noServer: true });
+
+const session = new Map();
+
+wss.on('connection', (client) => {
+	const uuid = crypto.randomUUID();
+	session.set(uuid, client);
+
+	client.send(JSON.stringify({ type: 'handshake', payload: { clientId: uuid } }));
+	console.log('client connected', uuid);
+});
+
+server.on('upgrade', (req, socket, head) => {
+	wss.handleUpgrade(req, socket, head, function done(ws) {
+		wss.emit('connection', ws, req);
+	});
+});
+
+const port = process.env.NODE_ENV === 'prod' ? process.env.PORT : 3000;
+server.listen(port, () => {
+	console.log(`listening on *:${port}`);
+});
